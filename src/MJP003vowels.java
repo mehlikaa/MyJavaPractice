4c65bc609467e9c40312be12a0825cb0de8446ee
mehlika <mehlika.balaban@gmail.com>
2022-07-02 17:55:44 -0700

    How many vowels and consonantare there?

--- src/MJP003vowels.java
+++ src/MJP003vowels.java
@@ -1,24 +1,47 @@
-import javax.sound.midi.Soundbank;
+
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class MJP003vowels {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int count;
+        int blank;
 
-        System.out.println("Metninizi giriniz : ");
-        String str= scan.nextLine();
-
-        List<String> list= List.of(str.toLowerCase().split(""));
-        System.out.println("List : "+list);
-
-        int count=0;
-        for (int i = 0; i <list.size(); i++) {
-            if(list.get(i).contains("a")||list.get(i).contains("e")||list.get(i).contains("i")||list.get(i).contains("o")||list.get(i).contains("u")) {
-            count++;
+        System.out.print("Metninizi giriniz : ");
+        String str = scan.nextLine();
+        // Kisa yontemle,  CharAt ile
+        System.out.println("/nStr : " + str + "\nstr.lenght : " + str.length());
+        count = 0;
+        blank = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                blank++;
+            }
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                count++;
             }
         }
-        System.out.println(" Kelimemizde : "+count+"adet vowel bulunmaktadir.");
+        System.out.println(" Kelimemizde : " + count + " adet vowel bulunmaktadir.");
+        System.out.println(" Kelimemizde : " + (str.length() - count - blank) + " adet consonant bulunmaktadir.\n" + blank + " adet bosluk bulunuyor.");
+
+
+//        //List kullanilarak
+//        List<String> list = List.of(str.toLowerCase().split(""));
+//        System.out.println("/n----LIST ----\nList : " + list);
+//
+//        count = 0;
+//        blank=0;
+//        for (int i = 0; i < list.size(); i++) {
+//            if (str.charAt(i) == ' ') {
+//                blank++;
+//            }
+//            if (list.get(i).contains("a") || list.get(i).contains("e") || list.get(i).contains("i") || list.get(i).contains("o") || list.get(i).contains("u")) {
+//                count++;
+//            }
+//        }
+//        System.out.println(" Kelimemizde : " + count + " adet vowel bulunmaktadir.");
+//        System.out.println(" Kelimemizde : " + (list.size()- count - blank) + " adet consonant bulunmaktadir.\n" + blank + " adet bosluk bulunuyor.");
+//
     }
-}
+    }
