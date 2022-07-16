839b17a67b80d10f31e557f1dcfd3b5ce683d9ab
mehlika <mehlika.balaban@gmail.com>
2022-07-16 14:53:36 -0700

    DoWhile Scope

--- src/Mjp012DoWhileLoop.java
+++ src/Mjp012DoWhileLoop.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Mjp012DoWhileLoop {
+    //Bir sayi isteyin ve girilen sayi sifir olana kadar sayilari toplayin ve 0'a basildiginda toplami yazdirin.
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         int topla = 0,x =0;
@@ -11,5 +12,14 @@ public class Mjp012DoWhileLoop {
         }while(x!=0);
         System.out.println("topla = " + topla);
 
+        //Kullanicidan toplamak icin sayi isteyin ve toplam 100’e ulasincaya kadar devam istemeyi ettirin.
+        // Toplam 500’e ulastiginda veya gectiginde toplami ve kac sayi girildigini yazdirin
+        int topla2 = 0,x2 =0;
+        do {
+            System.out.print("Bir sayi giriniz : ");
+            x2=scan.nextInt();
+            topla2+=x2;
+        }while(topla2<100);
+        System.out.println("topla2 = " + topla2);
     }
 }
